0b7a33e0bb74981c7eb27c398b2d32c92b1fc733
Haylee Thomas-Kuhlmann <hay.design.lee@gmail.com>
2020-10-21 16:54:59 -0500

    Working application with student search, student add, student info, student grades

--- hackbright_web.py
+++ hackbright_web.py
@@ -11,11 +11,43 @@ app = Flask(__name__)
 def get_student():
     """Show information about a student."""
 
-    github = "jhacks"
+    github = request.args.get('github')
 
     first, last, github = hackbright.get_student_by_github(github)
 
-    return "{} is the GitHub account for {} {}".format(github, first, last)
+    project_list = hackbright.get_grades_by_github(github)
+
+    html = render_template("student_info.html", first=first, last=last, github=github, project_list=project_list)
+
+    return html
+
+
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching for a student."""
+
+    return render_template("student_search.html")
+
+
+@app.route("/student-add", methods=['POST'])
+def student_add():
+    '''Add a student.'''
+
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    github = request.form.get('github')
+
+    hackbright.make_new_student(first_name, last_name, github)
+
+    return render_template("added_student.html", first_name=first_name, last_name=last_name, github=github)
+
+
+@app.route("/student-add-form")
+def add_student_form():
+    """Show form for adding for a student."""
+
+    return render_template("add_a_student.html")
+
 
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
